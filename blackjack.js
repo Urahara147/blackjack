15f0b4baf324c63ec32170a77054e5038305e7dd
Urahara147 <120706241+Urahara147@users.noreply.github.com>
2024-12-18 02:19:54 +0300

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -1,22 +1,39 @@
-let firstCard = Math.floor(Math.random() * 12)
-let secondCard = Math.floor(Math.random() * 12)
-let cards = [firstCard, secondCard]
+let cards = []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let sum = firstCard + secondCard
+let sum = 0
+
+function getRandomCard(param) {
+  let randomNumber = Math.floor(Math.random()* 13 ) + 1
+  if (randomNumber > 10) {
+    return 10
+  } else if (randomNumber === 1) {
+    return 11
+  }
+  else {
+    return randomNumber
+  }
+}
 
 function startGame(param) {
+  isAlive = true
+  
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]  
+
+sum = firstCard + secondCard
+  
   renderGame()
 }
 function renderGame(param) {
-  sumEl.textContent = "sum: " + sum
- cardsEl.textContent = "cards "
-
-  for (let i = 0; i < cards.length; i++) {
+  sumEl.textContent = "Sum: " + sum
+  cardsEl.textContent = "Cards: " 
+  for (var i = 0; i < cards.length; i++) {
   cardsEl.textContent += cards[i] + " "
  }
   if (sum <= 20) {
@@ -36,8 +53,16 @@ messageEl.textContent = message
 
 }
 function newCard(param) {
-let card = Math.floor(Math.random() * 12)
+let card = getRandomCard()
 sum += card
 cards.push(card)
 renderGame()
+
 }
+// function newGame(param) {
+
+//   sumEl.textContent = "sum: "
+// cardsEl.textContent = "cards: "
+// sum = 0
+// }
+console.log(cards)
